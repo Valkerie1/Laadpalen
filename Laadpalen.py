002348cffe5b9a6ca75ae373e769af1b1667dc24
Valkerie1 <91312670+Valkerie1@users.noreply.github.com>
2021-10-11 13:44:29 +0200

    Update Laadpalen.py

--- Laadpalen.py
+++ Laadpalen.py
@@ -4,12 +4,12 @@ import geopandas as gpd
 import pandas as pd
 from streamlit_folium import folium_static
 
-df = gpd.read_file('https://github.com/Valkerie1/Laadpalen/blob/main/countries.geojson')
+#df = gpd.read_file('https://github.com/Valkerie1/Laadpalen/blob/main/countries.geojson')
 df1 = pd.read_csv('https://github.com/Valkerie1/Laadpalen/blob/main/Life_Expectancy_Data.csv')
 
-df_who = df.merge(df1, left_on='ADMIN', right_on='Country')
-df_who.head()
-
+#df_who = df.merge(df1, left_on='ADMIN', right_on='Country')
+df_who = df1
+'''
 m = folium.Map(tiles='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', 
                attr='Mapbox attribution')
 
@@ -23,3 +23,4 @@ folium.Choropleth(geo_data=df_who, name='geometry',
 folium.features.ClickForMarker(popup=None).add_to(m)
 
 folium_static(m)
+'''
