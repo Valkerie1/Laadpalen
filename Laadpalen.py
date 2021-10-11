78d0f8b892728874789041f26f9dddc6445b5827
Valkerie1 <91312670+Valkerie1@users.noreply.github.com>
2021-10-11 17:44:50 +0200

    Update Laadpalen.py

--- Laadpalen.py
+++ Laadpalen.py
@@ -4,34 +4,11 @@ import geopandas as gpd
 import pandas as pd
 from streamlit_folium import folium_static
 import plotly.express as px
-
-from urllib.request import urlopen
-import json
-with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-    counties = json.load(response)
-
-
-df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
-                   dtype={"fips": str})
+import plotly.graph_objects as go
 
 
 
-df = px.data.election()
-geojson = px.data.election_geojson()
-
-fig = px.choropleth_mapbox(df, geojson=geojson, color="winner",
-                           locations="district", featureidkey="properties.district",
-                           center={"lat": 45.5517, "lon": -73.7073},
-                           mapbox_style="carto-positron", zoom=9)
-dropdown_buttons = [
-    {'method': 'update', 'label': 'All','args': [{'visible': [True, True, True]}]},
-    {'method': 'update', 'label': 'Joly','args': [{'visible': [True, False, False]}]},
-    {'method': 'update', 'label': 'Coderre','args': [{'visible': [False, True, False]}]},
-    {'method': 'update', 'label': 'Bergeron','args': [{'visible': [False, False, True]}]}]
-
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.update_layout({'updatemenus':[{'type': 'dropdown', 'buttons': dropdown_buttons}]})
 
 
-st.plotly_chart(fig)
+
 
