8960031cf9e4ddb526768a9dad952708742e4aea
Valkerie1 <91312670+Valkerie1@users.noreply.github.com>
2021-10-12 14:52:51 +0200

    Update Laadpalen.py

--- Laadpalen.py
+++ Laadpalen.py
@@ -171,13 +171,13 @@ st.markdown("***")
 
 col1, col2, col3= st.columns(3)
 
-col1.write(
-with st.expander('Opties:'):
-         col1, col2 = st.columns(2)
-         laadtijd_rangeselection_max = col1.slider('Selecteer het bereik van de oplaad tijd:',0,4000,600,100)
-         laadtijd_selectbox = col2.selectbox('Laat opmerkingen zien:', ['Gemiddelde','Mediaan','Beide'], index=2)
-laadtijd_rangeselection_min = 0
-)
+with col1:
+         with st.expander('Opties:'):
+                  col1, col2 = st.columns(2)
+                  laadtijd_rangeselection_max = col1.slider('Selecteer het bereik van de oplaad tijd:',0,4000,600,100)
+                  laadtijd_selectbox = col2.selectbox('Laat opmerkingen zien:', ['Gemiddelde','Mediaan','Beide'], index=2)
+         laadtijd_rangeselection_min = 0
+
          
 
 histogram_selector = st.selectbox('Selecteer een grafiek:',['Laad tijd','Tijd aan de laadpaal', 'Kansdichtheid'], index=0) 
