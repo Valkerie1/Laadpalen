3608ed179e5dad61fea2c71085b95bd2d5122e3a
Valkerie1 <91312670+Valkerie1@users.noreply.github.com>
2021-10-12 14:20:46 +0200

    Update Laadpalen.py

--- Laadpalen.py
+++ Laadpalen.py
@@ -12,7 +12,7 @@ import plotly.figure_factory as ff
 import scipy
 
 st.set_page_config(page_title = 'Streamlit Dashboard', layout= 'wide')
-st.header("<h1 style='text-align: center; color: red;'>Some title</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: red;'>Some title</h1>", unsafe_allow_html=True)
 
 # laden van api
 countrycode = 'NL'
